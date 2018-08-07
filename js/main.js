3dae5cf9cd59b0468ed492b815c59e21236963ab
Ben Bridgeman <ben_bridgeman@hotmail.co.uk>
2018-08-07 18:01:38 +0100

    Add containers to restaurant items to allow responsive images

--- js/main.js
+++ js/main.js
@@ -160,27 +160,36 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
 createRestaurantHTML = (restaurant) => {
 	const li = document.createElement('li');
 
+	const imageContainer = document.createElement('div');
+	imageContainer.className = 'restaurant-image-wrapper';
+
 	const image = document.createElement('img');
 	image.className = 'restaurant-img';
 	image.src = DBHelper.imageUrlForRestaurant(restaurant);
-	li.append(image);
+	imageContainer.append(image);
+	li.append(imageContainer);
+
+	const infoContainer = document.createElement('div');
+	infoContainer.className = 'info-wrapper';
 
 	const name = document.createElement('h1');
 	name.innerHTML = restaurant.name;
-	li.append(name);
+	infoContainer.append(name);
 
 	const neighborhood = document.createElement('p');
 	neighborhood.innerHTML = restaurant.neighborhood;
-	li.append(neighborhood);
+	infoContainer.append(neighborhood);
 
 	const address = document.createElement('p');
 	address.innerHTML = restaurant.address;
-	li.append(address);
+	infoContainer.append(address);
 
 	const more = document.createElement('a');
 	more.innerHTML = 'View Details';
 	more.href = DBHelper.urlForRestaurant(restaurant);
-	li.append(more);
+	infoContainer.append(more);
+
+	li.append(infoContainer);
 
 	return li;
 };
